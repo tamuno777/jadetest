e1e0264265e2419b9c4cfd544ae21177b6351d07
tamuno777 <davisbeulah07@gmail.com>
2023-07-24 07:38:50 -0700

    nav bar

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -15,30 +15,15 @@ function Navs() {
     
   return (
     <>
-      {['md', ].map((expand) => (
-        <Navbar key={expand} expand={expand} className="  nav-container w-100  ">
-          <Container fluid>
-            {/* <Navbar.Brand className='d-block' >
-              
-
-            </Navbar.Brand> */}
-
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="start"
-              className=" text-light hello"
-              // min={true}
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body style={{display:"block",paddingTop:"0px",height:"0px"}}>
+      
+        <Navbar  className="  nav-container w-100 " >
+          <Container fluid className='d-block'> 
+           
+             
+            
               <div  > <h1 className='text-center pt-0 hello '><strong>JADE MINARA</strong></h1></div>
 
-                <Nav className="justify-content-center flex-grow-1 fs-5  nav-content hello ">
+                <Nav className="justify-content-center d-flex flex-grow-1 fs-5  nav-content hello ">
 
                   <div className='link'  ><Navbutton title="Home" to="/Home" /></div>
                   <div className='link'  ><Navbutton title="Store" to="/Store" /></div>
@@ -48,11 +33,10 @@ function Navs() {
                 </Nav>
 
                   
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
+            
           </Container>
         </Navbar>
-      ))}
+    
     </>
   );
 }
